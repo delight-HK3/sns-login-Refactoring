dfe21301e78804df186853b797a6ab8b44e543aa
leedabean <dabin310153@naver.com>
2025-01-07 08:51:15 +0900

    Update SnsLoginService.java

--- src/main/java/com/example/snslogin/service/sns/SnsLoginService.java
+++ src/main/java/com/example/snslogin/service/sns/SnsLoginService.java
@@ -2,13 +2,18 @@ package com.example.snslogin.service.sns;
 
 import org.springframework.stereotype.Service;
 
+import com.example.snslogin.dto.authResponse;
+import com.example.snslogin.dto.userResponse;
 import com.example.snslogin.type.UserType;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @Service
 public interface SnsLoginService {
-    UserType getServiceName(); // sns 타입 확인
+    UserType getServiceType(); // sns 타입 확인
     String getOauthRedirectURL(); // sns 로그인 form 호출
-    
+    String getAccessToken(String authorizationCode); // accesstoken 호출
+    JsonNode getUserInfo(String accessToken); // 로그인 시도한 유저 정보 출력
+
     default UserType type(){
         if(this instanceof GoogleLoginServiceImpl){ // 구글 로그인
             return UserType.GOOGLE;
