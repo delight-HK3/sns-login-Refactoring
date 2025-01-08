df1346076047287e7e8fca8e647430cba8323759
leedabean <dabin310153@naver.com>
2025-01-09 08:14:02 +0900

    type default function delete
    - type default function delete
    - Usertype return function add

--- src/main/java/com/example/snslogin/service/sns/SnsLoginService.java
+++ src/main/java/com/example/snslogin/service/sns/SnsLoginService.java
@@ -1,7 +1,5 @@
 package com.example.snslogin.service.sns;
 
-import javax.imageio.IIOException;
-
 import org.springframework.stereotype.Service;
 
 import com.example.snslogin.type.UserType;
@@ -10,15 +8,5 @@ import com.example.snslogin.type.UserType;
 public interface SnsLoginService {
     String getRedirectURL(); // sns 로그인 form 호출
     
-    default UserType type(){
-        if(this instanceof GoogleLoginServiceImpl){ 
-            return UserType.GOOGLE; // 구글 로그인
-        } else if(this instanceof KakaoLoginServiceImpl){ 
-            return UserType.KAKAO; // 카카오 로그인
-        } else if(this instanceof NaverLoginServiceImpl){ 
-            return UserType.NAVER; // 네이버 로그인
-        } else{
-            return UserType.NONE; // 존재하지 않는 sns 타입
-        }
-    }
+    UserType getUserType(); // 로그인한 sns 타입 리턴
 } 
