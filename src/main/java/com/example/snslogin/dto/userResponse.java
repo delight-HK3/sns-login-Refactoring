ac0796943d27aa9a013f94c501254dba775997a3
leebean <dabin310153@naver.com>
2025-04-05 15:35:15 +0900

    builder pattern add

--- src/main/java/com/example/snslogin/dto/userResponse.java
+++ src/main/java/com/example/snslogin/dto/userResponse.java
@@ -1,23 +1,39 @@
 package com.example.snslogin.dto;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
-
 @Getter
-@Setter
 public class userResponse {
 
     // 유저 정보
-    private int no;                 // 회원번호
-    private String id;              // 아이디
-    private String pwd;             // 비밀번호      
-    private String email;           // 이메일
-    private String nickname;        // 닉네임
-    private String name;            // 이름
-    private String age;             // 나이
-    private String gender;          // 성별
-    private String birthday;        // 생일날짜
-    private String birthyear;       // 생일년도
-    private String mobile;          // 모바일
-    private String mobile_e164;
+    private final int no;                 // 회원번호
+    private final String id;              // 아이디
+    private final String pwd;             // 비밀번호      
+    private final String email;           // 이메일
+    private final String nickname;        // 닉네임
+    private final String name;            // 이름
+    private final String age;             // 나이
+    private final String gender;          // 성별
+    private final String birthday;        // 생일날짜
+    private final String birthyear;       // 생일년도
+    private final String mobile;          // 모바일
+    private final String mobile_e164;
+
+    @Builder
+    public userResponse(int no, String id, String pwd, String email, String nickname
+                        , String name, String age, String gender, String birthday, String birthyear
+                        , String mobile, String mobile_e164){
+        this.no = no;
+        this.id = id;
+        this.pwd = pwd;
+        this.email = email;
+        this.nickname = nickname;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.birthyear = birthyear;
+        this.mobile = mobile;
+        this.mobile_e164 = mobile_e164;
+    }
 }
