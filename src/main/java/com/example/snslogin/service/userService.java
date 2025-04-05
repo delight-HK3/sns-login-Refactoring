c459d8728d050b4ffa4cea2bedc36a158e2b89ac
leebean <dabin310153@naver.com>
2025-04-05 15:34:46 +0900

    restTemplate -> restClient change

--- src/main/java/com/example/snslogin/service/userService.java
+++ src/main/java/com/example/snslogin/service/userService.java
@@ -1,48 +1,52 @@
 package com.example.snslogin.service;
 
 import java.io.IOException;
+
 import java.util.List;
 
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
+import com.example.snslogin.dto.userResponse;
 import com.example.snslogin.service.sns.SnsLoginService;
 import com.example.snslogin.type.UserType;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
-@RequiredArgsConstructor
 public class userService {
     
     private final List<SnsLoginService> loginServices;
     private final HttpServletResponse response;
-    // sns 로그인 관련 property value
-    private final Environment environment;
-    private final RestTemplate restTemplate;
+    private final Environment environment; // propertyies파일 value
+    private final RestClient restClient;
+
+    public userService(List<SnsLoginService> loginServices, HttpServletResponse response
+                        , Environment environment, RestClient.Builder restClientBuilder){
+        this.loginServices = loginServices;
+        this.response = response;
+        this.environment = environment;
+        this.restClient = restClientBuilder.build(); // restClient 기본 설정
+    }
 
     // Authorization서버에 요청하여 accesstoken 획득 및 accesstoken으로 
     // Resource서버에 요청해 유저 정보 획득
-    public JsonNode snsLogin(UserType userType, String code){
+    public userResponse snsLogin(UserType snsType, String code){
+        SnsLoginService snsloginservice = this.findSnsType(snsType); 
         
-        String accessToken = this.getAccessToken(userType, code);
-        JsonNode useresponse = this.getUserInfo(userType, accessToken);
-        
-        System.out.println("useresponse "+userType+" : "+useresponse);
+        String accessToken = this.getAccessToken(snsType, code);
+        JsonNode jsonNode = this.getUserInfo(snsType, accessToken);
 
-        return useresponse;
+        userResponse result = snsloginservice.getResponse(jsonNode);
+
+        return result;
     } 
 
     // sns로그인 시도시 Authorization서버에서 제공하는 페이지 호출
@@ -69,6 +73,7 @@ public class userService {
 
     // Authorization 서버를 통해 accesstoken 발급
     private String getAccessToken(UserType userType, String authorizationCode){
+        // HTTP 파라미터, 폼 데이터 등 다중 값 처리 용도
         MultiValueMap<String,Object> params = new LinkedMultiValueMap<>();
 
         // 각 sns별 Authorization 서버주소
@@ -80,14 +85,12 @@ public class userService {
         params.add("redirect_uri", environment.getProperty("spring.OAuth2."+userType+".callback-url"));
         params.add("grant_type", "authorization_code"); 
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        HttpEntity entity = new HttpEntity(params, headers);
-
-        // RestTemplate -> RestClient 도입고민 
-        ResponseEntity<JsonNode> responseNode = 
-            restTemplate.exchange(accesstokenUrl, HttpMethod.POST, entity, JsonNode.class);
+        ResponseEntity<JsonNode> responseNode = restClient.post()
+                                .uri(accesstokenUrl)
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .body(params)
+                                .retrieve()
+                                .toEntity(JsonNode.class);
 
         JsonNode accessTokenNode = responseNode.getBody();
         
@@ -96,15 +99,16 @@ public class userService {
 
     // Resource 서버를 통해 유저정보 발급
     private JsonNode getUserInfo(UserType userType, String accessToken) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + accessToken);
-
+       
         String resourceUrl = environment.getProperty("spring.OAuth2."+userType+".resource-url");
 
-        HttpEntity entity = new HttpEntity(headers);
-
-        // RestTemplate -> RestClient 도입고민 
-        return restTemplate.exchange(resourceUrl, HttpMethod.GET, entity, JsonNode.class).getBody();
+        JsonNode result = restClient.get()
+                                .uri(resourceUrl)
+                                .header("Authorization",  "Bearer " + accessToken)
+                                .retrieve()
+                                .body(JsonNode.class);
+                                
+        return result;
     }
 
 }
